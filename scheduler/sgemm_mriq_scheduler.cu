7b69e0cac943c3c222bbbb2a6b88075e5cb24bb9
Aditya <aweditya@gmail.com>
2023-10-15 14:31:08 +0530

    Signalling after unlock

--- scheduler/sgemm_mriq_scheduler.cu
+++ scheduler/sgemm_mriq_scheduler.cu
@@ -276,10 +276,10 @@ int main(int argc, char **argv)
             pthread_mutex_lock(&(sgemm_args[0].kcb->kernel_lock));
             printf("Got the sgeem lock\n");
             sgemm_args[0].kcb->state = RUNNING;
-            pthread_cond_signal(&(sgemm_args[0].kcb->kernel_signal));
-            printf("sgemm signalled\n");
             pthread_mutex_unlock(&(sgemm_args[0].kcb->kernel_lock));
             printf("Released the sgeem lock\n");
+            pthread_cond_signal(&(sgemm_args[0].kcb->kernel_signal));
+            printf("sgemm signalled\n");
         }
         else
         {
@@ -287,10 +287,10 @@ int main(int argc, char **argv)
             pthread_mutex_lock(&(mriq_args[0].kcb->kernel_lock));
             printf("Got the mriq lock\n");
             mriq_args[0].kcb->state = RUNNING;
-            pthread_cond_signal(&(mriq_args[0].kcb->kernel_signal));
-            printf("mriq signalled\n");
             pthread_mutex_unlock(&(mriq_args[0].kcb->kernel_lock));
             printf("Released the mriq lock\n");
+            pthread_cond_signal(&(mriq_args[0].kcb->kernel_signal));
+            printf("mriq signalled\n");
         }
 
         if (sgemm_args[0].kcb->slices == 0 && mriq_args[0].kcb->slices == 0)
