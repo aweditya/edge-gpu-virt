78aebaf43a3c8af11938999f21e24b6f7e072884
Aditya <aweditya@gmail.com>
2024-01-11 16:00:28 +0530

    Logging mechanism working using unified memory

--- custom-benchmarking/clockBlock.cu
+++ custom-benchmarking/clockBlock.cu
@@ -6,25 +6,22 @@
 #include <pthread.h>
 #include "errchk.h"
 
-#define NUM_KERNELS 2                                // Number of kernels
+#define NUM_KERNELS 16                               // Number of kernels
 #define KERNEL_TIME 1000L                            // Time to run kernel for (in ms)
 #define LOGGING_INTERVAL 10                          // Logging interval (in ms)
 #define LOGGING_DURATION (NUM_KERNELS * KERNEL_TIME) // Logging duration (in ms)
 
 typedef struct kernel_thread_args
 {
-    int kernelID;
+    int *threadsRunning;
     long clock_count;
     cudaStream_t stream;
 } kernel_thread_args_t;
 
-__device__ int d_threadsRunning[NUM_KERNELS];
-
 // This is a kernel that does no real work but runs at least for a specified number of clocks
-__global__ void clockBlock(long clock_count, int kernelID)
+__global__ void clockBlock(long clock_count, int *threadsRunning)
 {
-    atomicAdd(&d_threadsRunning[kernelID], 1); // Increment when thread starts
-    printf("[%d] %d\n", kernelID, d_threadsRunning[kernelID]);
+    atomicAdd(threadsRunning, 1); // Increment when thread starts
 
     unsigned int start_clock = (unsigned int)clock();
 
@@ -46,36 +43,33 @@ __global__ void clockBlock(long clock_count, int kernelID)
         clock_offset = (long)(end_clock - start_clock);
     }
 
-    atomicSub(&d_threadsRunning[kernelID], 1); // Decrement when thread starts
-    printf("[%d] %d\n", kernelID, d_threadsRunning[kernelID]);
+    atomicSub(threadsRunning, 1); // Decrement when thread starts
 }
 
 // Periodically report number of threads for each kernel
-__host__ void reportThreadsRunning()
+__host__ void reportThreadsRunning(int *allThreadsRunning)
 {
-    int h_threadsRunning[NUM_KERNELS];
-    checkCudaErrors(cudaMemcpyFromSymbolAsync(h_threadsRunning, d_threadsRunning, sizeof(int) * NUM_KERNELS));
-    // for (int i = 0; i < NUM_KERNELS; ++i)
-    // {
-    //     printf("%d\t", h_threadsRunning[i]);
-    // }
-    // printf("\n");
+    for (int i = 0; i < NUM_KERNELS; ++i)
+    {
+        printf("%d\t", allThreadsRunning[i]);
+    }
+    printf("\n");
 }
 
 void *kernel_threadfunction(void *args)
 {
     kernel_thread_args_t *kernel_args = (kernel_thread_args_t *)args;
-    printf("[%ld] launched kernel #%d\n", (unsigned long)pthread_self(), kernel_args->kernelID);
-    clockBlock<<<1, 32, 0, kernel_args->stream>>>(kernel_args->clock_count, kernel_args->kernelID);
+    clockBlock<<<32, 128, 0, kernel_args->stream>>>(kernel_args->clock_count, kernel_args->threadsRunning);
     return NULL;
 }
 
 void *logger_threadfunction(void *args)
 {
+    int *allThreadsRunning = (int *)args;
     const int iterations = LOGGING_DURATION / LOGGING_INTERVAL;
     for (int i = 0; i < iterations; ++i)
     {
-        reportThreadsRunning();
+        reportThreadsRunning(allThreadsRunning);
         usleep(LOGGING_INTERVAL * 1000);
     }
 
@@ -89,23 +83,22 @@ int main()
     int clockRate = prop.clockRate;
     int clock_count = KERNEL_TIME * clockRate;
 
-    // Initialise d_threadsRunning
-    int *_d_threadsRunning;
-    checkCudaErrors(cudaGetSymbolAddress((void **)&_d_threadsRunning, d_threadsRunning));
-    checkCudaErrors(cudaMemset(_d_threadsRunning, 0, sizeof(int) * NUM_KERNELS));
+    // Allocate unified memory
+    int *allThreadsRunning = NULL;
+    checkCudaErrors(cudaMallocManaged((void **)&allThreadsRunning, sizeof(int) * NUM_KERNELS));
 
     kernel_thread_args_t args[NUM_KERNELS];
     pthread_t kernel_threads[NUM_KERNELS], logger_thread;
 
     for (int i = 0; i < NUM_KERNELS; ++i)
     {
-        args[i].kernelID = i;
+        args[i].threadsRunning = &allThreadsRunning[i];
         args[i].clock_count = clock_count;
         checkCudaErrors(cudaStreamCreate(&args[i].stream));
     }
 
     /* Start logger thread */
-    if (pthread_create(&logger_thread, NULL, logger_threadfunction, NULL) != 0)
+    if (pthread_create(&logger_thread, NULL, logger_threadfunction, allThreadsRunning) != 0)
     {
         perror("pthread_create failed");
     }
@@ -137,6 +130,7 @@ int main()
         checkCudaErrors(cudaStreamDestroy(args[i].stream));
     }
 
+    checkCudaErrors(cudaFree(allThreadsRunning));
     checkCudaErrors(cudaDeviceReset());
 
     return 0;
