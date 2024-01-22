12cad7c074412fb0fecbcb2ce62d56c567e2636e
Aditya <aweditya@gmail.com>
2024-01-22 17:29:20 +0530

    Reporting SM ID as well

--- custom-benchmarking/clockBlock.cu
+++ custom-benchmarking/clockBlock.cu
@@ -6,22 +6,40 @@
 #include <pthread.h>
 #include "errchk.h"
 
-#define NUM_KERNELS 16                               // Number of kernels
+#define NUM_KERNELS 2                                // Number of kernels
 #define KERNEL_TIME 1000L                            // Time to run kernel for (in ms)
 #define LOGGING_INTERVAL 10                          // Logging interval (in ms)
 #define LOGGING_DURATION (NUM_KERNELS * KERNEL_TIME) // Logging duration (in ms)
 
+typedef struct kernel_metadata
+{
+    int thread_args;
+    unsigned int smID;
+} kernel_metadata;
+
 typedef struct kernel_thread_args
 {
-    int *threadsRunning;
+    kernel_metadata *metadata;
     long clock_count;
     cudaStream_t stream;
-} kernel_thread_args_t;
+} kernel_thread_args;
+
+// Get ID of SM on which kernel thread is running
+__device__ unsigned int get_smid(void)
+{
+    unsigned int smID;
+    asm("mov.u32 %0, %smid;" : "=r"(smID));
+    return smID;
+}
 
 // This is a kernel that does no real work but runs at least for a specified number of clocks
-__global__ void clockBlock(long clock_count, int *threadsRunning)
+__global__ void clockBlock(long clock_count, kernel_metadata *metadata)
 {
-    atomicAdd(threadsRunning, 1); // Increment when thread starts
+    if (threadIdx.x == 0 && threadIdx.y == 0 && threadIdx.z == 0)
+    {
+        atomicAdd(&metadata->thread_args, 1); // Increment when thread starts
+        metadata->smID = get_smid();
+    }
 
     unsigned int start_clock = (unsigned int)clock();
 
@@ -43,33 +61,36 @@ __global__ void clockBlock(long clock_count, int *threadsRunning)
         clock_offset = (long)(end_clock - start_clock);
     }
 
-    atomicSub(threadsRunning, 1); // Decrement when thread starts
+    if (threadIdx.x == 0 && threadIdx.y == 0 && threadIdx.z == 0)
+    {
+        atomicSub(&metadata->thread_args, 1); // Decrement when thread starts
+    }
 }
 
 // Periodically report number of threads for each kernel
-__host__ void reportThreadsRunning(int *allThreadsRunning)
+__host__ void reportThreadsRunning(kernel_metadata *allMetadata)
 {
     for (int i = 0; i < NUM_KERNELS; ++i)
     {
-        printf("%d\t", allThreadsRunning[i]);
+        printf("(%u, %d)\t", allMetadata[i].smID, allMetadata[i].thread_args);
     }
     printf("\n");
 }
 
 void *kernel_threadfunction(void *args)
 {
-    kernel_thread_args_t *kernel_args = (kernel_thread_args_t *)args;
-    clockBlock<<<32, 128, 0, kernel_args->stream>>>(kernel_args->clock_count, kernel_args->threadsRunning);
+    kernel_thread_args *kernel_args = (kernel_thread_args *)args;
+    clockBlock<<<32, 128, 0, kernel_args->stream>>>(kernel_args->clock_count, kernel_args->metadata);
     return NULL;
 }
 
 void *logger_threadfunction(void *args)
 {
-    int *allThreadsRunning = (int *)args;
+    kernel_metadata *allMetadata = (kernel_metadata *)args;
     const int iterations = LOGGING_DURATION / LOGGING_INTERVAL;
     for (int i = 0; i < iterations; ++i)
     {
-        reportThreadsRunning(allThreadsRunning);
+        reportThreadsRunning(allMetadata);
         usleep(LOGGING_INTERVAL * 1000);
     }
 
@@ -84,21 +105,21 @@ int main()
     int clock_count = KERNEL_TIME * clockRate;
 
     // Allocate unified memory
-    int *allThreadsRunning = NULL;
-    checkCudaErrors(cudaMallocManaged((void **)&allThreadsRunning, sizeof(int) * NUM_KERNELS));
+    kernel_metadata *allMetadata = NULL;
+    checkCudaErrors(cudaMallocManaged((void **)&allMetadata, sizeof(int) * NUM_KERNELS));
 
-    kernel_thread_args_t args[NUM_KERNELS];
+    kernel_thread_args args[NUM_KERNELS];
     pthread_t kernel_threads[NUM_KERNELS], logger_thread;
 
     for (int i = 0; i < NUM_KERNELS; ++i)
     {
-        args[i].threadsRunning = &allThreadsRunning[i];
+        args[i].metadata = &allMetadata[i];
         args[i].clock_count = clock_count;
         checkCudaErrors(cudaStreamCreate(&args[i].stream));
     }
 
     /* Start logger thread */
-    if (pthread_create(&logger_thread, NULL, logger_threadfunction, allThreadsRunning) != 0)
+    if (pthread_create(&logger_thread, NULL, logger_threadfunction, allMetadata) != 0)
     {
         perror("pthread_create failed");
     }
@@ -130,7 +151,7 @@ int main()
         checkCudaErrors(cudaStreamDestroy(args[i].stream));
     }
 
-    checkCudaErrors(cudaFree(allThreadsRunning));
+    checkCudaErrors(cudaFree(allMetadata));
     checkCudaErrors(cudaDeviceReset());
 
     return 0;
